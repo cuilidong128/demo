5dc79df0d4ad47e867853ba217fca175e60a7d59
mrcuilidong@163.com <lidong.cui@cotticoffee.com>
2026-02-11 16:25:35 +0800

    12

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // 测试
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
